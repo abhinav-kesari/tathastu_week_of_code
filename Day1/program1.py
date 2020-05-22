0bf822d1fa7f3c5cd7b509f82eb0f4848140a855
abhinav-kesari <abhinavkesari16@gmail.com>
2020-05-22 11:06:50 +0530

    Program1

---
+++ Day1/program1.py
@@ -0,0 +1,12 @@
+name = input("Name : ")
+branch = input("Branch : ")
+gender = input("Gender : ")
+college = input("College : ")
+age  = int(input("Age : "))
+print("\nName:",name)
+print("\nBranch:",branch)
+print("\nGender:",gender)
+print("\nCollege:",college)
+print("\nAge:",age)
+
+
